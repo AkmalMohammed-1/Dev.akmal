c8f237faab1f8e8581a0ad98e654554886407d02
AkmalMohammed-1 <adamakmal_mohammedabdul@srmap.edu.in>
2026-02-25 14:18:32 +0530

    Updates

---
+++ _next/static/3kVJ-8EULRz6I-hA-aarX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Dev.akmal//_next/:path+",
+        "destination": "/Dev.akmal/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/b0eb1ed813687fac.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,22170,c=>{c.v(t=>Promise.all(["static/chunks/ac4b0a3303885e1a.js","static/chunks/90cbe4f9cdedbbc5.js"].map(t=>c.l(t))).then(()=>t(26502)))}]);
